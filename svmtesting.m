ae2eb4aebcf90e3ae8d069c80691f7f503cf78b4
W Li <wqli@hotmail.co.uk>
2013-06-02 02:12:43 +0100

    new file:   svmtesting.m

---
+++ svmtesting.m
@@ -0,0 +1,9 @@
+diary('~/output/plainsvms.log');
+parfor schemeInd = 1:10
+    for click = 1:50
+        expFactory(schemeInd, click, 'svm-ign', 1);
+        expFactory(schemeInd, click, 'svm-all-fea', 1);
+        expFactory(schemeInd, click, 'svm-all-loc', 1);
+    end
+end
+diary off;
